4a697c8546eac50319c58838f1c6bfb9e2275185
Craig Davison <craigadavison77@gmail.com>
2023-04-10 17:12:58 +0100

    refactored

--- js/accordion.js
+++ js/accordion.js
@@ -1,58 +1,70 @@
+// accordion js
 const d = document;
-const accordions = d.querySelectorAll(".accordion_container");
+const accordions = d.querySelectorAll(".accordion");
 const accordion_sections = d.querySelectorAll(".accordion_section");
 const accordion_headers = d.querySelectorAll(".accordion_head");
+let accordion_active_sections = d.querySelectorAll(".accordion_section.active");
 
-accordion_sections.forEach((section) => {
-  if (section.classList.contains("active")) {
-    const section_body = section.querySelector(".accordion_body");
-    const section_body_height = section_body.offsetHeight;
-    section.querySelector(".accordion_body_container").style.maxHeight =
-      section_body_height;
-  }
+const accordion_set_section_height = (section) => {
+  const section_body = section.querySelector(".accordion_body");
+  const height = `${section_body.offsetHeight}px`;
+  section.querySelector(".accordion_body_container").style.maxHeight = height;
+};
+
+accordion_active_sections.forEach((section) =>
+  accordion_set_section_height(section)
+);
+
+window.addEventListener("resize", () => {
+  accordion_active_sections = d.querySelectorAll(".accordion_section.active");
+  accordion_active_sections.forEach((section) =>
+    accordion_set_section_height(section)
+  );
 });
+
+const accordion_toggle_active = (clicked_section) => {
+  const body_container = clicked_section.querySelector(
+    ".accordion_body_container"
+  );
+  if (clicked_section.classList.contains("active")) {
+    clicked_section.classList.remove("active");
+    body_container.style.removeProperty("max-height");
+  } else {
+    accordion_set_section_height(clicked_section);
+    clicked_section.classList.add("active");
+  }
+};
 
 accordion_headers.forEach((header) => {
   header.addEventListener("click", (e) => {
     // get clicked accordion section
-    const accordion = header.closest(".accordion_container");
-    const behavior = accordion.getAttribute("data-behavior");
-    console.log(behavior);
-    const clicked_section = header.closest(".accordion_section");
-    const clicked_body = clicked_section.querySelector(".accordion_body");
-    const clicked_body_height = clicked_body.offsetHeight;
+    const this_accordion = header.closest(".accordion");
+    console.log(this_accordion);
+    const this_accordion_sections =
+      this_accordion.querySelectorAll(".accordion_section");
+    const behavior = this_accordion.getAttribute("data-behavior");
+    const toggle = this_accordion.getAttribute("data-toggle-open");
 
-    // if the clicked section is not the current active section
-    if (behavior !== "stay-open" && !clicked_section.hasAttribute("active")) {
-      // select the accordion container that was clicked within
-      const accordion = e.target.closest(".accordion_container");
-      // get all accordion sections within the selected accordion
-      const sections = accordion.querySelectorAll(".accordion_section");
-      clicked_section.querySelector(
-        ".accordion_body_container"
-      ).style.maxHeight = clicked_body_height;
-      clicked_section.classList.add("active");
-      // for each section
-      for (const section of sections) {
+    const clicked_section = header.closest(".accordion_section");
+
+    if (behavior === "stay-open") {
+      accordion_toggle_active(clicked_section);
+    } else {
+      for (const section of this_accordion_sections) {
         // check to see if the section clicked on
         if (section !== clicked_section) {
-          section.classList.remove("active");
           section
             .querySelector(".accordion_body_container")
             .style.removeProperty("max-height");
+          section.classList.remove("active");
+        } else {
+          if (toggle) {
+            accordion_toggle_active(clicked_section);
+          } else {
+            accordion_set_section_height(section);
+            section.classList.add("active");
+          }
         }
-      }
-    } else {
-      if (clicked_section.classList.contains("active")) {
-        clicked_section.classList.remove("active");
-        clicked_section
-          .querySelector(".accordion_body_container")
-          .style.removeProperty("max-height");
-      } else {
-        clicked_section.querySelector(
-          ".accordion_body_container"
-        ).style.maxHeight = clicked_body_height;
-        clicked_section.classList.add("active");
       }
     }
   });
